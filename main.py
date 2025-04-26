80d2e25af57fdedebb86799ba191fd699cd96b7b
LALOLEVY <EDUARDOENTRERPRICE@GMAIL.COM>
2025-04-25 22:04:08 -0600

    Update main.py

--- main.py
+++ main.py
@@ -25,3 +25,15 @@ def meli_webhook():
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=10000)
+import os
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return "Bot is running!"
+
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 5000))  # Render asigna el puerto
+    app.run(host="0.0.0.0", port=port)
